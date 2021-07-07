a43c39d58c3e1d04c5c03585ef5f21db20a97806
faurori <faurori@gmail.com>
2021-07-07 16:38:01 -0400

    Added query to  UserRepo

--- src/main/java/com/revature/repository/UserRepository.java
+++ src/main/java/com/revature/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.revature.repository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.models.User;
@@ -14,6 +16,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	public Optional<User> findByUsername(String username);
 	
+	@Query("select u from USERS where u.username = :username and u.password = :password ")
+	List<User> checkUsernamePassword(String username, String password);
 	
 	
 }
