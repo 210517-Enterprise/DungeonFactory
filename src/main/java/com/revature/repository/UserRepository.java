146b580cab8ab3f175a58c0eaa6ded9a2c3a3c32
faurori <faurori@gmail.com>
2021-07-06 21:06:50 -0400

    Added UserRepo

---
+++ src/main/java/com/revature/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.revature.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.models.User;
+
+@Repository 
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	
+	public Optional<User> findByUsername(String username);
+	
+	
+	
+}
