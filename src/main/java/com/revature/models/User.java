1d50b34be3c3396401cbdc8674db1a99cbec9367
faurori <faurori@gmail.com>
2021-07-06 21:03:23 -0400

    Added more user annotations

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -1,14 +1,15 @@
 package com.revature.models;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,13 +22,16 @@ public class User {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="u_id")
+	@Column(name="u_id", nullable =false, unique=true, updatable = false)
 	private int id;
 	
+	@Length(min=1)
+	@Pattern(regexp="[a-zA-Z][a-zA-Z0-9]*")
 	private String username;
 	
+	@NotEmpty
 	private String password;
 	
-	@OneToMany(mappedBy="owner")
-	private List<Character> characters;
+//	@OneToMany(mappedBy="owner")
+//private List<Character> characters;
 }
