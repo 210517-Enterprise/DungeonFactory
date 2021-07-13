8e4738712369e0aa48f9fd44e7122e9a56573fa6
faurori <faurori@gmail.com>
2021-07-13 19:13:50 -0400

    Fixed User class error

--- src/main/java/com/revature/user/User.java
+++ src/main/java/com/revature/user/User.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
@@ -32,6 +33,7 @@ public class User {
 	
 	@NotEmpty
 	private String password;
+	
 
 	public User(String username, String password) {
 		super();
