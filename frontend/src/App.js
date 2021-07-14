7697bbc7aed46c9401a66272025acefab4aa5fe6
FBThornton <97fthornton@gmail.com>
2021-07-14 16:25:09 -0400

    Renamed files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,98 +9,82 @@ import {
 
 import Home from './Home/Home'
 import Login from './User/LoginFunc'
-import Logout from './User/Logout'
+import Logout from './User/LogoutFunc'
 import Register from './User/RegisterFunc'
 import Characters from './Character/CharacterList'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import D20 from './D20.gif'
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {currentUser: null};
-        this.handleLogin = this.handleLogin.bind(this);
-        this.handleLogout = this.handleLogout.bind(this);
-    }
-    
-    handleLogin(user) {
-        this.setState({currentUser: user});
-    }
+export default function App() {
+    const [user, updateUser] = useState(null);
 
-    handleLogout() {
-        this.setState({currentUser: null});
-    }
-
-    async componentDidMount() {
-        let response = await fetch('http://localhost:8080/user/auth', {method: 'GET', credentials: 'include'})
-
-        try {
-            this.setState({currentUser: await response.json()});
-        } catch(err){
-            console.log("No user");
+    useEffect(() => {
+        async function getUser() {
+            let response = await fetch('http://localhost:8080/user/auth', {method: 'GET', credentials: 'include'});
+            let json = await response.json();
+            if(json){
+                updateUser(json);
+            }
         }
+        getUser();
+    }, [])
+
+    let links = []
+
+    if (user){
+        links.push({
+            to: "/characters",
+            label: "Characters"
+        });
+        links.push({
+            to: "/logout",
+            label: "Logout"
+        });
+    } else {
+        links.push({
+            to: "/login",
+            label: "Login"
+        });
+        links.push({
+            to: "/register",
+            label: "Register"
+        });
     }
 
-    render() { 
-        let links = []
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <div class="navbar">
+                    <div class="left-nav">
 
-        if (this.state.currentUser) {
-            links.push({
-                to: "/characters",
-                label: "Characters"
-            });
-            links.push({
-                to: "/logout",
-                label: "Logout"
-            });
-        } else {
-            links.push({
-                to: "/login",
-                label: "Login"
-            });
-            links.push({
-                to: "/register",
-                label: "Register"
-            });
-        }
-        
-        return (
-            <div className="app">
-                <BrowserRouter>
-                    <div class="navbar">
-                        <div class="left-nav">
+                        <Link to="/">  <img src={D20} alt="tinylogo"/> </Link>
 
-                            <Link to="/">  <img src={D20} alt="tinylogo"/> </Link>
-
-                        </div>
-                        
-                        
-                        <div class= "right-nav">
-                            {links.map(link => <li key={link.to}><Link to={link.to}>{link.label}</Link></li>)}
-                        </div>
                     </div>
-                    <Switch>
-                        <Route path="/login">
-                            <Login user={this.state.currentUser} onLogin={this.handleLogin}/>
-                        </Route>
-                        <Route path="/register">
-                            <Register user={this.state.currentUser} onLogin={this.handleLogin}/>
-                        </Route>
-                        <Route path="/characters">
-                            <Characters user={this.state.currentUser}/>
-                        </Route>
-                        <Route path="/logout">
-                            <Logout onLogout={this.handleLogout}/>
-                        </Route>
-                        <Route path="/">
-                            <Home/>
-                        </Route>
-                    </Switch>
-                </BrowserRouter>
-            </div>
-        )
-    }
-}
+                    
+                    
+                    <div class= "right-nav">
+                        {links.map(link => <li key={link.to}><Link to={link.to}>{link.label}</Link></li>)}
+                    </div>
+                </div>
+                <Switch>
+                    <Route path="/login">
+                        <Login user={user} updateUser={updateUser}/>
+                    </Route>
+                    <Route path="/register">
+                        <Register user={user} updateUser={updateUser}/>
+                    </Route>
+                    <Route path="/characters">
+                        <Characters user={user}/>
+                    </Route>
+                    <Route path="/logout">
+                        <Logout user={user} updateUser={updateUser}/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    )
 
-export default App;
+}
--- frontend/src/User/Logout.jsx
+++ frontend/src/User/Logout.jsx
@@ -1,18 +1,12 @@
-import React from 'react';
+import { useEffect } from "react";
 import { Redirect } from 'react-router';
 
-class Logout extends React.Component {
-    constructor(props) {
-        super(props);
-
+export default function Logout({user, updateUser}) {
+    
+    useEffect((props) => {
         fetch('http://localhost:8080/user/logout', {method: 'GET', credentials: 'include'});
-
-        this.props.onLogout();
-    }
-
-    render() {
-        return <div><Redirect to="../Home"/></div>
-    }
+        props.updateUser(null);
+    }, [])
+    
+    return <div><Redirect to="../Home"/></div>
 }
-
-export default Logout;
