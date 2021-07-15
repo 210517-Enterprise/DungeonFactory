d68ead67d0b7ea30123b15bb2453195bcb5f629d
FBThornton <97fthornton@gmail.com>
2021-07-15 14:42:51 -0400

    Adjusted logic for pushing error message to screen

--- frontend/src/User/Register.jsx
+++ frontend/src/User/Register.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {useForm} from 'react-hook-form';
 import {Redirect} from 'react-router';
 
-export default function Register({user, onLogin}) {
+export default function Register({user, updateUser}) {
     const [errorMessage, updateErrorMessage] = useState("");
     const {register, handleSubmit} = useForm({
         defaultVaules: {
@@ -23,10 +23,10 @@ export default function Register({user, onLogin}) {
             const response = await fetch('http://localhost:8080/user/register', requestInfo)
 
             if(response.status !== 200){
-                updateErrorMessage(response.text)
+                updateErrorMessage(await response.text())
             } else {
                 const user = await response.json();
-                onLogin(user);
+                updateUser(user);
             }
 
         } else {
@@ -39,6 +39,7 @@ export default function Register({user, onLogin}) {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <h1>REGISTER</h1>
                 <label>{errorMessage}</label>
+                <br/>
                 <label>
                     Username:  
                     <input {... register("username", {required: true})}/>
