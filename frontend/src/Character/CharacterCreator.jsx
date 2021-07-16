93c96b62b62980d49b168ee208f2b1c8ada59d28
faurori <faurori@gmail.com>
2021-07-16 14:28:29 -0400

    Fixed character attribute string bug

--- frontend/src/Character/CharacterCreator.jsx
+++ frontend/src/Character/CharacterCreator.jsx
@@ -82,33 +82,33 @@ export default function CharacterCreator() {
 
             <div class="abilities">
                 <label for="constitution">Constitution</label>
-                <input {...register("constitution", { required: true })}>
+                <input type="number" {...register("constitution", { required: true })}>
                 </input>
                 
                 <label for="charisma">Charisma</label>
-                <input {...register("charisma", { required: true })}>
+                <input type="number" {...register("charisma", { required: true })}>
                 </input>
 
                 <label for="dexterity">Dexterity</label>
-                <input {...register("dexterity", { required: true })}>
+                <input type="number" {...register("dexterity", { required: true })}>
                 </input>
 
                 <label for="intelligence">Intelligence</label>
-                <input {...register("intelligence", { required: true })}>
+                <input type="number" {...register("intelligence", { required: true })}>
                 </input>
 
                 <label for="wisdom">Wisdom</label>
-                <input {...register("wisdom", { required: true })}>
+                <input type="number" {...register("wisdom", { required: true })}>
                 </input>
 
                 <label for="strength">Strength</label>
-                <input {...register("strength", { required: true })}>
+                <input type="number" {...register("strength", { required: true })}>
                 </input>
             </div>
 
             <div class="characterName">
             <label for="characterName">Character Name</label>
-                <input {...register("characterName", { required: true })}>
+                <input type="text" {...register("characterName", { required: true })}>
                 </input>
 
             </div>
