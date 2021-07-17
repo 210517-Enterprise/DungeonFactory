80cfa4fd1744df5eb09354ead6bdcb9f5d40fd02
FBThornton <97fthornton@gmail.com>
2021-07-17 17:39:35 -0400

    Removed second CSS

--- frontend/src/Character/CharacterView.jsx
+++ frontend/src/Character/CharacterView.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from 'react-router';
 import {classToPng, raceToPng} from './CharacterImages';
 // Css
-import './CharacterView2.css';
+import './CharacterView.css';
 
 
 export default function CharacterView() {
