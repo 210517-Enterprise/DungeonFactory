31f9add4e2707fe40916c32499eb64925fe409d1
FBThornton <97fthornton@gmail.com>
2021-07-17 17:36:49 -0400

    Added new CSS

--- frontend/src/Character/CharacterView.jsx
+++ frontend/src/Character/CharacterView.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from 'react-router';
 import {classToPng, raceToPng} from './CharacterImages';
 // Css
-import './CharacterView.css';
+import './CharacterView2.css';
 
 
 export default function CharacterView() {
@@ -14,7 +14,6 @@ export default function CharacterView() {
             method: 'GET',
             credentials: 'include'
         };
-        console.log(id)
         fetch(`http://localhost:8080/character/${id}`, requestInfo)
             .then(response => response.json())
             .then(json => updateCharacter(json));
@@ -25,28 +24,60 @@ export default function CharacterView() {
             <h1>{character.characterName}</h1>
             <div className="wrapper">
                 <span className="classRace">
-                    <div className="cr">
+                    <div>
                         <img src={raceToPng(character.race)} alt={character.race}/>
                         <h3>Race: {character.race}</h3>
                     </div>
-                    <div className="cr">
+                    <div>
                         <img src={classToPng(character.characterClass)} alt={character.characterClass}/>
                         <h3>Class: {character.characterClass}</h3>
                     </div>
                 </span>
                 <span className="attributes">
-                    <h3 className="attHeader">Attributes</h3>
                     <div>
-                        <div>
-                            <span>STR: {character.strength}</span>
-                            <span>CON: {character.constitution}</span>
-                            <span>INT: {character.intelligence}</span>
-                        </div>
-                        <div>
-                            <span>WIS: {character.wisdom}</span>
-                            <span>DEX: {character.dexterity}</span>
-                            <span>CHA: {character.charisma}</span>
-                        </div>
+                        <h3>Attributes</h3>
+                        <span>
+                            <p>STR: {character.strength}</p>
+                            <p>CON: {character.constitution}</p>
+                            <p>INT: {character.intelligence}</p>
+                        </span>
+                        <span>
+                            <p>WIS: {character.wisdom}</p>
+                            <p>DEX: {character.dexterity}</p>
+                            <p>CHA: {character.charisma}</p>
+                        </span>
+                    </div>
+                </span>
+                <span className="personalityfandt">
+                    <div>
+                        <h3>Personality</h3>
+                        <p>{character.personality}</p>
+                    </div>
+                    <div>
+                        <h3>Feats & Traits</h3>
+                        <p>{character.featAndTraits}</p>
+                    </div>
+                </span>
+                <span className="backalign">
+                    <div>
+                        <p><b>Background:</b> {character.background}</p>
+                    </div>
+                    <div>
+                        <p><b>Alignment:</b> {character.alignment}</p>
+                    </div>
+                </span>
+                <span className="idealbondflaw">
+                    <div>
+                        <h3>Ideals</h3>
+                        <p>{character.ideals}</p>
+                    </div>
+                    <div>
+                        <h3>Bonds</h3>
+                        <p>{character.bonds}</p>
+                    </div>
+                    <div>
+                        <h3>Flaws</h3>
+                        <p>{character.flaws}</p>
                     </div>
                 </span>
             </div>
