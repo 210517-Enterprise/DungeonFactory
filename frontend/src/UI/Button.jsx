e09e02969189600ed6bd6aaa136f92b978f68e18
Derek <21209173+ddinh@users.noreply.github.com>
2021-07-17 15:02:53 -0500

    Add ability score rolling

--- frontend/src/UI/Button.jsx
+++ frontend/src/UI/Button.jsx
@@ -4,7 +4,7 @@ export const Button = styled.button`
   flex: 0;
   padding: 12px 18px;
   border-radius: 4px;
-  background: #39ABFE;
+  background: ${props => props.background || "#39ABFE"};
   font-weight: 600;
   font-family: inherit;
   font-size: 18px;
